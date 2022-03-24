c0e4b4bbf4631fbda301901341e7ea1540fd177c
Grego <dgarciadani@gmail.com>
2022-03-24 13:44:06 -0300

    Spring Web update

--- springWebMVC/src/main/java/com/example/springWebMVC/controller/UserController.java
+++ springWebMVC/src/main/java/com/example/springWebMVC/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.springWebMVC.controller;
 import com.example.springWebMVC.domain.User;
 import com.example.springWebMVC.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private UserService userService = new UserService();
 
-    @GetMapping("/")
+    @GetMapping("/user1")
     public User createUser(){
         return userService.createUser("Marcos",23);
     }
 
-    @GetMapping("/maps")
+    @GetMapping("/user2")
     public User createUser2(){
         return userService.createUser("Pablo",45);
     }
+
 }
- 
