eb188e192a28c6b1767c9dec1e65c107b8a6cfde
Grego Garcia <82552333+Dgarciadani@users.noreply.github.com>
2022-04-14 18:28:02 -0300

    I modified the Masterclass

--- MasterClass_Javier_Integrative_Class/src/main/java/com/grego/MasterClass_Javier_Integrative_Class/model/dtos/PersonDTO.java
+++ MasterClass_Javier_Integrative_Class/src/main/java/com/grego/MasterClass_Javier_Integrative_Class/model/dtos/PersonDTO.java
@@ -1,5 +1,8 @@
 package com.grego.MasterClass_Javier_Integrative_Class.model.dtos;
 
+import lombok.Data;
+
+@Data
 public class PersonDTO {
     private Integer id;
     private String name;
--- MasterClass_Javier_Integrative_Class/src/test/java/com/grego/MasterClass_Javier_Integrative_Class/MasterClassJavierIntegrativeClassApplicationTests.java
+++ MasterClass_Javier_Integrative_Class/src/test/java/com/grego/MasterClass_Javier_Integrative_Class/MasterClassJavierIntegrativeClassApplicationTests.java
@@ -1,15 +1,35 @@
 package com.grego.MasterClass_Javier_Integrative_Class;
 
+import com.grego.MasterClass_Javier_Integrative_Class.model.Person;
+import com.grego.MasterClass_Javier_Integrative_Class.model.dtos.PersonDTO;
+import com.grego.MasterClass_Javier_Integrative_Class.service.PersonService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MasterClassJavierIntegrativeClassApplicationTests {
-/*
 
-	@Test
-	void contextLoads() {
-	}
-*/
 
+    @Autowired
+    private PersonService personService;
+    @Autowired
+    private ModelMapper modelMapper;
+    @Test
+    public void savePerson() {
+        Person person = new Person();
+        person.setName("Javier");
+        person.setSurname("Pate");
+        PersonDTO personDTO = modelMapper.map(person, PersonDTO.class);
+        Assertions.assertSame(personService.savePerson(personDTO).getName(), person.getName());
+        Assertions.assertNotNull(personService.savePerson(personDTO));
+
+    }
+
+    @Test
+    public void findById() {
+        Assertions.assertNotNull(personService.findPersonById(1));
+    }
 }
